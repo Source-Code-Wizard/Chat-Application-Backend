8480f40de2a3f4953d0517505dcc10b1e7aadda1
TSALAPATIS ALEXANDROS <atsalapa@netcompany.com>
2026-01-04 12:13:37 +0200

    Initial commit

---
+++ src/main/java/com/poc/chat/application/backend/ChatApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.poc.chat.application.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplicationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplicationBackendApplication.class, args);
+    }
+
+}
